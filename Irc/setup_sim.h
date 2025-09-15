9be711f7a5de2209d83181b51ddeb0cd9fe44cfc
Axyst2k4 <axyst2k4@gmail.com>
2025-09-15 12:19:35 +0700

    a

--- Irc/setup_sim.h
+++ Irc/setup_sim.h
@@ -1,2 +1,15 @@
 #ifndef _SETUP_LIB_SIM_H_
 #define _SETUP_LIB_SIM_H_
+
+//------------------------------------------------connect_broker------------------------------------------------------------------
+#define client_index 0
+#define clientID "1234"
+#define server_type 0
+#define server_addr "tcp://<địa chỉ>:<port>"
+#define keepalive_time 600
+#define clean_session 1
+#define topic "abc"
+
+#define qos 0
+#define PHONE_NUMBER "0000000000"
+char com[256]; askjsafdjklasjdf
