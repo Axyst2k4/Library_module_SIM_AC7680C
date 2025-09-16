2f8533a519bd0db81e272f1033b844a13925f9e2
Axyst2k4 <axyst2k4@gmail.com>
2025-09-16 22:39:36 +0700

    Auto-sync: file changes

--- Src/Rx_lib_SIM.c
+++ Src/Rx_lib_SIM.c
@@ -1,11 +1,5 @@
 #include "Rx_lib_SIM.h"
-#include "setup_sim.h"
-char com[50];
-char buffer_response[BUFFER_SIZE];
-char buffer_calib[BUFFER_SIZE];
 
-bool received_flag = false;
-int req_length =strlen(topic); 
 
 
 //=====================================work_flow===========================================
@@ -158,30 +152,22 @@ void SysTick_Init(volatile uint32_t set_time) {
     
     }
 }
-void Wait_Response( ResponseType_e *response_correct, volatile uint32_t set_time, UART_HandleTypeDef* huart){
+int Wait_Response( ResponseType_e *response_correct, volatile uint32_t set_time, UART_HandleTypeDef* huart){
     SysTick_Init(set_time);
     if (g_receiver.response_count !=0){ //response not correct
         for(int i = 0; i < g_receiver.response_count;i++){
             if(g_receiver.index_cmd[i] == response_correct ){
-                return;
-            } else if( i == (g_receiver.response_count-1)){
-                g_state = g_state -  1;  
+                return 0;
+            } else if( i == (g_receiver.response_count-1)){ 
+                g_state = STATE_ERROR;
+                return 10;
             }
         }
     } else { //Timeout
-        Lib_SIM_Setup(huart);
-
+        g_state = STATE_ERROR;
+        return 10;
     }
 }
- void Error_Connect(void){
-
- }
-
-
-
-
-
-
 
 
 
